9c74e313cffc3bf099e8909ccc9370ca7c1fed49
selin pir <113732977+selinpir@users.noreply.github.com>
2023-12-06 22:55:48 +0300

    c#-stack

---
+++ stack_kod/stack_kod/Program.cs
@@ -0,0 +1,70 @@
+﻿using stack_kod;
+using System;
+namespace Program
+{
+    class program
+    {
+        static void Main(string[] args) 
+        {
+            /* stack stk = new stack();
+             string cikan;
+             stk.push(4);
+             Console.WriteLine("4 eklendi.Stack boyutu "+ stk.count);
+             stk.push(5);
+             Console.WriteLine("5 eklendi.Stack boyutu " + stk.count);
+             stk.push(6);
+             Console.WriteLine("6 eklendi.Stack boyutu " + stk.count);
+             cikan= stk.pop().ToString();
+             Console.WriteLine(cikan + " çıkarıldı.Stack boyutu " + stk.count);
+             cikan = stk.pop().ToString();
+             Console.WriteLine(cikan + " çıkarıldı.Stack boyutu " + stk.count);
+             cikan = stk.pop().ToString();
+             Console.WriteLine(cikan + " çıkarıldı.Stack boyutu " + stk.count);
+             stk.push(9);
+             Console.WriteLine("9 eklendi.Stack boyutu " + stk.count);
+             stk.clear();
+             Console.WriteLine("stack temizlendi. Stack boyutu" + stk.count);
+             stk.push(12);
+             Console.WriteLine("12 eklendi.Stack boyutu " + stk.count);
+              */
+
+
+            //palindrom örnek
+            stack stk = new stack();
+            Console.WriteLine("bir kelime giriniz");
+            string kelime = Console.ReadLine();
+
+            bool palindromMu = true;
+
+            for (int i = 0; i < kelime.Length; i++)
+            {
+                stk.push(kelime.Substring(i, 1)); // kelimenin karakterleri parçalandı ve stacke eklendi
+            }
+
+            int pozisyon = 0;
+            string karakter;
+            while(stk.count > 0) 
+            {
+                karakter = stk.pop().ToString();
+
+                if(kelime.Substring(pozisyon,1)!=karakter)
+                {
+                    palindromMu = false;
+                    break;
+                }
+                pozisyon++;
+
+            }
+            if (palindromMu)
+            {
+                Console.WriteLine(kelime + " bir palindrom kelimedir.");
+            }
+            else
+            {
+                Console.WriteLine(kelime + " bir palindrom kelime değildir.");
+            }
+        }
+
+
+    }
+}
---
+++ stack_kod/stack_kod/stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack_kod
+{
+    internal class stack
+    {
+        private int p_index; //stack pointer
+        private ArrayList list; // stack i tutması için bir arraylist
+
+        public stack()
+        { 
+        list=new ArrayList();
+            p_index = -1;
+        }
+        public void push (object item)
+        {
+            list.Add(item);
+            p_index++;
+
+        }
+        public object pop () //parametre almasına gerek yok en yukaridaki değeri döndürür.
+        {
+            object obj = list[p_index];
+            list.RemoveAt(p_index);
+            p_index--;
+            return obj ;
+        }
+        public void clear()
+        { 
+            list.Clear();
+            p_index = -1; //index sıfırlanır.
+        }
+
+        public int count
+        {
+            get{ return list.Count; } //stack boyutu döndürülür.
+        }
+
+    }
+    
+}
